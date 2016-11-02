6b6558b0da8f7e2bf449ba062090154fa84bdec6
sc0ttruss <scott.russell@inetmedia.co.uk>
2016-11-02 13:34:21 +0200

    fix the key creation script, removing unused code

--- recipes/genkey.rb
+++ recipes/genkey.rb
@@ -7,11 +7,9 @@ directory '/mnt/share/.ssh' do
 end
 
 bash 'generate_keys' do
-  user 'vagrant'
+  user 'root'
   cwd '/var/tmp'
   code <<-EOH
-  echo " ENV = ", `env`
-  echo " HOME = ", $HOME
   echo -e  'y\n'|ssh-keygen -t rsa -b 4096 -C vagrant@chefdemo -V +1024w1d -N '' -f /mnt/share/.ssh/id_rsa && cat /mnt/share/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
   EOH
 end
